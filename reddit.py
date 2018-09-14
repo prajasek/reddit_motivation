d0603ac9bd933a3603d66a9ddbaf7138217e469d
Prasanth Raja Sekaran <prasanth.dps@gmail.com>
2018-09-14 00:13:10 -0400

    added comments

--- reddit.py
+++ reddit.py
@@ -1,13 +1,6 @@
-
-# coding: utf-8
-
-# In[1]:
-
-import praw, pprint
+import praw
 import webbrowser
 
-
-# In[2]:
 
 reddit = praw.Reddit(client_id = "dv4AghN-m6GxfA",
                     client_secret = "lYi6i98Fc9Iqh8W9WdJfUdfadAY",
@@ -15,39 +8,31 @@ reddit = praw.Reddit(client_id = "dv4AghN-m6GxfA",
                     username = "wannastro", 
                     user_agent = "get_motivated")
 
-
-# In[3]:
-
+#https://www.reddit.com/r/getmotivated
 subreddit = reddit.subreddit("getmotivated")
 
-
-# In[20]:
-
 post_dict = {"title" : [], "id" : [] , "url" : []}
+
+#hot/new posts
 for post in subreddit.hot(limit=5):
     post_dict["title"].append(post.title)
     post_dict["id"].append(post.id)
     post_dict["url"].append(post.url)
-for i in range(1):
+    
+#add one random post 
+for i in range(1): 
     post_dict["title"].append(subreddit.random().title)
     post_dict["id"].append(subreddit.random().id)
     post_dict["url"].append(subreddit.random().url)
 
 
-# In[21]:
-
-# Hot 3 post +  1 random post
-
+# Hot/new 3 post +  1 random post
 hot_ten_links = [(post_title,"https://www.reddit.com/r/GetMotivated/comments/"+ post_id) for post_title, post_id in zip(post_dict["title"][2:],post_dict["id"][2:])]
 
-
-# In[22]:
-
+# Open these links in default browser
 for links in hot_ten_links:
     webbrowser.open(links[1], new=1)
 
-
-# In[ ]:
 
 
 
